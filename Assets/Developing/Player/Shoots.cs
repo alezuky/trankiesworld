f0428161435329acc369307f15e26ef157c03e87
Alexandre Greluk Szykman <alezuky@gmail.com>
2016-06-09 16:46:57 -0300

    Fixed bugs on player selection

--- Assets/Developing/Player/Shoots.cs
+++ Assets/Developing/Player/Shoots.cs
@@ -31,7 +31,10 @@ public class Shoots : MonoBehaviour
 		int shootHash = Animator.StringToHash("shoot");
 
 	IEnumerator SafetyTime() {
-		yield return new WaitForSeconds(3.0f);
+		float waitTime = 0.1f;
+		if (string.Compare (Application.loadedLevelName, "Spaceship_Level") == 0)
+			waitTime = 3.0f;
+		yield return new WaitForSeconds(waitTime);
 		safetyTime = true;		
 	}
 
