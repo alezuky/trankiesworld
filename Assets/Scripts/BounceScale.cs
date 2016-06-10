9d82122adf827442c9b1d2efb076bc943f25f85c
Alexandre Greluk Szykman <alezuky@gmail.com>
2016-06-10 15:53:07 -0300

    Improved Timer and Target visibility in SpaceShip Level

--- Assets/Scripts/BounceScale.cs
+++ Assets/Scripts/BounceScale.cs
@@ -5,7 +5,7 @@ public class BounceScale : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		iTween.ScaleAdd(gameObject,iTween.Hash("amount",new Vector3(1f,1f,0),"time",1,"looptype",iTween.LoopType.pingPong,"easetype",iTween.EaseType.linear,"delay",Random.Range(0,.4f)));
+		iTween.ScaleAdd(gameObject,iTween.Hash("amount",new Vector3(1f,1f,0),"time",1,"looptype",iTween.LoopType.pingPong,"easetype",iTween.EaseType.linear,"delay",Random.Range(0,0.4f)));
 	}
 
 }
