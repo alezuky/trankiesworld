36121d5434fad9fcc42839006713563cf1213e46
Alexandre Greluk Szykman <alezuky@gmail.com>
2016-06-13 18:17:25 -0300

    Reduced Training and credits duration to 3s

--- Assets/Scripts/ProvTrainingLevel.cs
+++ Assets/Scripts/ProvTrainingLevel.cs
@@ -11,7 +11,7 @@ public class ProvTrainingLevel : MonoBehaviour {
 	
 	IEnumerator LoadGame() {
 		Debug.Log("Carregando");
-		yield return new WaitForSeconds(5.0f);
+		yield return new WaitForSeconds(3.0f);
 		levelLoaded = true;
 		
 	}
