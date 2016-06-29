a9d9de4622cc794f10a7d4bfa05a477298705b82
Alexandre Greluk Szykman <alezuky@gmail.com>
2016-06-29 18:56:13 -0300

    Improved project settings and splash time for faster testing

--- Assets/Developing/Scripts/Credits.cs
+++ Assets/Developing/Scripts/Credits.cs
@@ -11,7 +11,7 @@ public class Credits : MonoBehaviour {
 	
 	IEnumerator LoadGame() {
 		Debug.Log("Carregando");
-		yield return new WaitForSeconds(3.0f);
+		yield return new WaitForSeconds(2.0f);
 		levelLoaded = true;
 		
 	}
