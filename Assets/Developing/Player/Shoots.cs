093ff02bc47dbcab23333a094d1b79016fe8b0e3
MARCOS-PC\marcos <marcossuzuki@gmail.com>
2016-09-02 13:54:54 -0300

    Alterações nos comandos por teclado para controlar os personagens. teclas: a= move para esquerda d= move para direita spaço = pula tecla 1 = alterna o controle para o player 1 tecla 2 = alterna o controle para o player 2 shift esquerdo = pausa ou continuo o jogo. movimento do mouse direciona a mira botão esquerdo do mouse atira

--- Assets/Developing/Player/Shoots.cs
+++ Assets/Developing/Player/Shoots.cs
@@ -69,104 +69,138 @@ public class Shoots : MonoBehaviour
 
 			firelocal = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
 			firetimer = firetimer + Time.deltaTime;
-			if (kinect.tracked == true) {
-			Point ();
+        /*
+        if (kinect.tracked == true) {
+			    Point ();
 			}
+            */
+        Point();
 
-		if (kinect.SetFire()) {
+        if (kinect.SetFire()) {
 			gameObject.SendMessageUpwards("Fire");
 			anim.SetTrigger(shootHash);						
 		}
 
+        if ((Input.GetButtonDown("Fire1_P1") || Input.GetButtonDown("Fire1_P2")) && gameObject.name == "Player_" + gameObject.GetComponent<CharController>().numplayer)
+        {
+            gameObject.SendMessageUpwards("Fire");
+            anim.SetTrigger(shootHash);
+        }
+       
+
+    }
+
+
+    public void Point()
+    {
+        Vector3 mouseDirection = (Input.mousePosition - gameObject.transform.position);
+        //bool mouseRaycast = Physics.Raycast(transform.position, mouseDirection, out hit);
+        //bool kinectRaycast = Physics.Raycast(transform.position, kinect.GetDirection(), out hit);
+        Vector3 useDirection = new Vector3();
+        useDirection = mouseDirection;
+        /*
+        if (mouseRaycast)
+        {
+            Debug.Log("Mouse");
+            useDirection = mouseDirection;
+        }          
+        else if (kinectRaycast)
+        {
+            Debug.Log("kinect");
+            useDirection = kinect.GetDirection();
+        }
+        */
+
+        if (Physics.Raycast(transform.position, useDirection, out hit))
+        {
+            //Physics.IgnoreCollision (instprojectile.collider, transform.root.collider);
+            //Debug.Log ("Poiting");
+            if (firstShoot)
+            {
+                rays[raycount] = Instantiate(ray, firelocal, Quaternion.LookRotation(useDirection)) as GameObject;
+                //rays[raycount].GetComponent<Renderer>().material.color = kinect.color;
+                //rays[raycount].GetComponent<Light>().color = kinect.color;
+                firstShoot = false;
+                Debug.Log("Instantiating Pointer");
+            }
+
+            rays[raycount].GetComponent<Renderer>().enabled = true;
+
+            /*scale = rays[raycount].transform.localScale;
+            scale.z = 50.0f * hit.distance;
+            Debug.Log("Hit distance: " + hit.distance);
+            rays[raycount].transform.localScale = scale;*/
+            //size += hit.distance;
+
+
+
+            // Rebating the raycast is not working
+            // That is why the if statement was set to "size < 0" -> it will never be accessed (because it does not work)
+            if (size < 0 && (Physics.Raycast (rays [raycount].transform.position, hit.normal, out hitRebate)) == true) {
+
+				if (rebate == false) {
+					PointRebate ();
+				} else {
+					RebateRebate ();
+				}
+
+			} else {
+				rebate = false;
+				size = hit.distance;
+
+				rays [raycount].transform.position = firelocal;
+				rays [raycount].transform.rotation = Quaternion.LookRotation (useDirection);
+
+				scale = rays [raycount].transform.localScale;
+				scale.z = 50.0f*hit.distance;
+				rays [raycount].transform.localScale = scale;
+
+			}
+
+		} else {
+			if (rays [raycount] != null) {
+				//Destroy (rays [raycount].gameObject);
+				//firstShoot = true;
+				rays [raycount].GetComponent<Renderer>().enabled = false;
+			}
+		}
+    }
+    
+
+
+	public void PointRebate ()
+	{
+
+		rays [raycount].transform.position = hit.point;
+		rays [raycount].transform.rotation = Quaternion.LookRotation (hitRebate.normal);
+
+		scale = rays [raycount].transform.localScale;
+		scale.z = hitRebate.distance;
+		rays [raycount].transform.localScale = scale;
+		size += hitRebate.distance;
+
+		rebate = true;
+	}
+
+	public void RebateRebate ()
+	{
+		if (Physics.Raycast (rays [raycount].transform.position, hitRebate.normal, out rebateRebate)) {
+			Physics.Raycast (rays [raycount].transform.position, rebateRebate.normal, out hitRebate);
+			rays [raycount].transform.position = hitRebate.point;
+			rays [raycount].transform.rotation = Quaternion.LookRotation (rebateRebate.normal);
+
+			scale = rays [raycount].transform.localScale;
+			scale.z = rebateRebate.distance;
+			rays [raycount].transform.localScale = scale;
+			size += rebateRebate.distance;
+
+		}
 	}
 
 
-		public void Point ()
-		{
-				if (Physics.Raycast (transform.position, kinect.GetDirection (), out hit)) {
-					//Physics.IgnoreCollision (instprojectile.collider, transform.root.collider);
-			Debug.Log ("Poiting");
-						if (firstShoot) {
-								rays [raycount] = Instantiate (ray, firelocal, Quaternion.LookRotation (kinect.GetDirection ())) as GameObject;
-								//rays[raycount].GetComponent<Renderer>().material.color = kinect.color;
-								//rays[raycount].GetComponent<Light>().color = kinect.color;
-								firstShoot = false;
-								Debug.Log ("Instantiating Pointer");
-						}
-
-						rays [raycount].GetComponent<Renderer>().enabled = true;
-
-						scale = rays [raycount].transform.localScale;
-						scale.z = hit.distance;
-						rays [raycount].transform.localScale = scale;
-						//size += hit.distance;
-
-						
-
-						// Rebating the raycast is not working
-						// That is why the if statement was set to "size < 0" -> it will never be accessed (because it does not work)
-						if (size < 0 && (Physics.Raycast (rays [raycount].transform.position, hit.normal, out hitRebate)) == true) {
-
-								if (rebate == false) {
-										PointRebate ();
-								} else {
-										RebateRebate ();
-								}
-						} else {
-								rebate = false;
-								size = hit.distance;
-
-								rays [raycount].transform.position = firelocal;
-								rays [raycount].transform.rotation = Quaternion.LookRotation (kinect.GetDirection ());
-
-								scale = rays [raycount].transform.localScale;
-								scale.z = hit.distance;
-								rays [raycount].transform.localScale = scale;
-
-
-						}
-				} else {
-						if (rays [raycount] != null) {
-								//Destroy (rays [raycount].gameObject);
-								//firstShoot = true;
-								rays [raycount].GetComponent<Renderer>().enabled = false;
-						}
-				}
-		}
-
-
-		public void PointRebate ()
-		{
-
-				rays [raycount].transform.position = hit.point;
-				rays [raycount].transform.rotation = Quaternion.LookRotation (hitRebate.normal);
-
-				scale = rays [raycount].transform.localScale;
-				scale.z = hitRebate.distance;
-				rays [raycount].transform.localScale = scale;
-				size += hitRebate.distance;
-
-				rebate = true;
-		}
-
-		public void RebateRebate ()
-		{
-				if (Physics.Raycast (rays [raycount].transform.position, hitRebate.normal, out rebateRebate)) {
-						Physics.Raycast (rays [raycount].transform.position, rebateRebate.normal, out hitRebate);
-						rays [raycount].transform.position = hitRebate.point;
-						rays [raycount].transform.rotation = Quaternion.LookRotation (rebateRebate.normal);
-
-						scale = rays [raycount].transform.localScale;
-						scale.z = rebateRebate.distance;
-						rays [raycount].transform.localScale = scale;
-						size += rebateRebate.distance;
-
-				}
-		}
-
-
 	public void Fire () {
-		if (safetyTime && firetimer > firecooldown) {
+        Vector3 mouseDirection = (Input.mousePosition - transform.position).normalized;
+        if (safetyTime && firetimer > firecooldown) {
 
 			GameObject objectProjectile = Instantiate (projectile, firelocal, transform.rotation) as GameObject;
 			Projectile proj = objectProjectile.GetComponent("Projectile") as Projectile;
@@ -177,13 +211,14 @@ public class Shoots : MonoBehaviour
 			// TODO set color of projectile ~> change mash renderer material
 
 			Transform instprojectile = objectProjectile.transform;
-			totalforce = (projectileforce / 10) * kinect.GetDirection () / kinect.transform.localScale.x;;
-			if (totalforce.x > projectileforce) {
+			totalforce = (projectileforce / 10) * mouseDirection /*kinect.GetDirection () / kinect.transform.localScale.x*/;
+            Debug.Log("Força: " + totalforce);
+            /*if (totalforce.x > projectileforce) {
 				totalforce.x = projectileforce;
 			}
 			if (totalforce.y > projectileforce) {
 				totalforce.y = projectileforce;
-			}
+			}*/
 
 			instprojectile.GetComponent<Rigidbody>().AddForce (totalforce);
 			AudioSource.PlayClipAtPoint(fire, transform.position);
