f71c39c2b476db626ef7b2b95c40873166d2b646
Alexandre Greluk Szykman <alezuky@gmail.com>
2016-09-28 17:16:09 -0300

    Destroys projectiles when hitting bounding boxes of SpaceShipLevel

---
+++ Assets/Developing/Platforms/Scripts/SpaceShipBoundingBox.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceShipBoundingBox : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision) {
+
+		GameObject other = collision.collider.gameObject;
+
+		if (other.tag == "Projectile") {
+			Destroy(other);
+		}
+	}
+}
