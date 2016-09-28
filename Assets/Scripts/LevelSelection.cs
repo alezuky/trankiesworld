8644a380635deb4cb800601a0d865d11b1b6e4e1
Alexandre Greluk Szykman <alezuky@gmail.com>
2016-09-28 17:10:21 -0300

    Added Timer before level start

--- Assets/Scripts/LevelSelection.cs
+++ Assets/Scripts/LevelSelection.cs
@@ -7,6 +7,22 @@ public class LevelSelection : MonoBehaviour {
 	public GameObject player1;
 	public GameObject player2;
 
+	private float timeLoad = 5;
+
+	private float counter;
+
+
+	public GameObject clock;
+	private GameObject instantiatedClock;
+
+	public GameObject clockPosition;
+
+
+	void Start()
+	{
+		counter = timeLoad;
+	}
+
 	void Update() {
 		Animator anim = GetComponent<Animator>();
 
@@ -17,11 +33,41 @@ public class LevelSelection : MonoBehaviour {
 		}
 	}
 
+
 	void OnTriggerEnter(Collider other) {
-		if (other.tag == "Player")
-		{
+		
+		if (other.tag == "Player") {
+			Debug.Log("Clock!");
+			instantiatedClock = GameObject.Instantiate (clock, clockPosition.transform.position , clockPosition.transform.rotation) as GameObject;
+			instantiatedClock.gameObject.GetComponent<Animator>().SetTrigger("EnemyActive");
+
+		}
+		}
+
+
+	void OnTriggerStay(Collider other) {
+
+		if (other.tag == "Player") {
+			counter -= Time.deltaTime;
+		}
+
+		if (other.tag == "Player" && counter < 0) {
 			scene.SelectLevel (other.gameObject, transform.parent.name);
 			other.gameObject.SetActive (false);
-		}
+
+			counter = timeLoad;
+			if (instantiatedClock) Destroy (instantiatedClock);
+			}
 	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.tag == "Player") {
+			counter = timeLoad;
+			if (instantiatedClock) Destroy (instantiatedClock);
+		}
+
+	}
+
+
+
 }
